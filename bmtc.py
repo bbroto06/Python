7fe9290641480823fd425445442b5d176da07998
Broto Bhattacharjee <b.broto06@gmail.com>
2016-07-21 17:01:43 +0530

    KIAS8 bus timings fares stoppages
    bmtc.py fetches :-
    1. bus timings, 
    2. fares, 
    3. stoppages 
    for bus # KIAS8 from the bmtc website (https://www.mybmtc.com/airportservices) which has been hardcoded as of now.

---
+++ bmtc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import requests
+from BeautifulSoup import BeautifulSoup
+import re, cgi
+
+url = "https://www.mybmtc.com/airportservices"
+response = requests.get(url)
+# parse html
+page = str(BeautifulSoup(response.content))
+
+
+def getURL(page):
+    """
+
+    :param page: html of web page (here: Python home page) 
+    :return: urls in that page 
+    """
+    start_link = page.find("a href")
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('KIAS-8', start_link)
+    end_quote = page.find('KIAS-9', start_quote + 1)
+    url = page[start_quote + 1: end_quote]
+    return url, end_quote
+
+while True:
+    url, n = getURL(page)
+    page = page[n:]
+    tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
+    if url:
+        no_tags = tag_re.sub('', url)
+        ready_for_web = cgi.escape(no_tags)
+        print ready_for_web
+    else:
+        break
